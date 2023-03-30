cc1c5fd049d4fba4e6e90162decd9a52dfeb47e8
chumahtung <qyd159@163.com>
2023-03-30 17:21:50 +0800

    fix: 部署脚本问题处理

--- publish.sh
+++ publish.sh
@@ -13,5 +13,5 @@ git push origin main
 if [ $1 = "all" ]; then
   pnpm publish -r --registry='https://nexus.yakies.cn/repository/hosted/'
 else
-  pnpm publish --dir packages/$1 -r --registry='https://nexus.yakies.cn/repository/hosted/'
+  pnpm --dir packages/$1 publish --registry='https://nexus.yakies.cn/repository/hosted/'
 fi
