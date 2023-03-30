5c15621a6bc7199d2f5ce8ae5913471544f36a74
chumahtung <qyd159@163.com>
2023-03-30 17:20:53 +0800

    feat: 继续优化部署脚本

--- publish.sh
+++ publish.sh
@@ -10,4 +10,8 @@ else
   echo "There are no changes to commit"
 fi
 git push origin main
-pnpm publish -r --registry='https://nexus.yakies.cn/repository/hosted/'
+if [ $1 = "all" ]; then
+  pnpm publish -r --registry='https://nexus.yakies.cn/repository/hosted/'
+else
+  pnpm publish --dir packages/$1 -r --registry='https://nexus.yakies.cn/repository/hosted/'
+fi
