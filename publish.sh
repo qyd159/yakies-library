0ad9d9a1ab691f75ebf30d6eeddf137792448e47
chumahtung <qyd159@163.com>
2023-03-30 17:06:41 +0800

    feat: 继续优化部署脚本

--- publish.sh
+++ publish.sh
@@ -1,7 +1,13 @@
-pnpm changeset version
-echo "请输入提交信息: "
-read commitMessage
-git add .
-git commit -m "${commitMessage}"
+#!/bin/bash
+if [[ $(git status --porcelain) ]]; then
+  pnpm changeset version
+  echo "请输入提交信息: "
+  read commitMessage
+  git add .
+  git commit -m "${commitMessage}"
+
+else
+  echo "There are no changes to commit"
+fi
 git push origin main
 pnpm publish -r --registry='https://nexus.yakies.cn/repository/hosted/'
