c53a7add310241907bd7b05e86bc37be43624c0e
chumahtung <qyd159@163.com>
2023-03-30 17:28:45 +0800

    fix: 部署脚本问题处理

--- publish.sh
+++ publish.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-if [[ $(git status --porcelain) ]]; then
-  pnpm changeset version
-  echo "请输入提交信息: "
-  read commitMessage
-  git add .
-  git commit -m "${commitMessage}"
-
-else
-  echo "There are no changes to commit"
-fi
-git push origin main
+# if [[ $(git status --porcelain) ]]; then
+#   pnpm changeset version
+#   echo "请输入提交信息: "
+#   read commitMessage
+#   git add .
+#   git commit -m "${commitMessage}"
+#   git push origin main
+# else
+#   echo "There are no changes to commit"
+# fi
 if [ $1 = "all" ]; then
   pnpm publish -r --registry='https://nexus.yakies.cn/repository/hosted/'
 else
-  echo 'pnpm --dir packages/{$1} publish --registry='https://nexus.yakies.cn/repository/hosted/' --access=public'
+  echo 'pnpm --dir packages/$1 publish --registry='https://nexus.yakies.cn/repository/hosted/' --access=public'
   pnpm --dir packages/$1 publish --registry='https://nexus.yakies.cn/repository/hosted/' --access=public
 fi
