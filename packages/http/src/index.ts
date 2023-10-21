7f52916141e76969d838d2409aba02d0d3d4fae9
chumahtung <qyd159@163.com>
2023-10-21 10:43:55 +0800

    feat: export token  utilities

--- packages/http/src/index.ts
+++ packages/http/src/index.ts
@@ -1,5 +1,6 @@
 import defHttp from './utils/http/axios'
 export * from './utils/http/axios';
+export * from './utils/token';
 export * from './types';
 export * from './request';
 export default defHttp
