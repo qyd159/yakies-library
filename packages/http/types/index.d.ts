b1cae63e1b55579066e52af85d7415cdfaae1113
chumahtung <qyd159@163.com>
2024-02-25 21:11:37 +0800

    fix: ts declaration file output dir

--- packages/http/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-import defHttp from './utils/http/axios';
-export * from './utils/http/axios';
-export * from './utils/token';
-export * from './types';
-export * from './request';
-export default defHttp;
--- packages/http/types/utils/http/axios/helper.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function joinTimestamp<T extends boolean>(join: boolean, restful: T): T extends true ? string : object;
-declare type Recordable<T = any> = Record<string, T>;
-/**
- * @description: Format request parameter time
- */
-export declare function formatRequestDate(params: Recordable): void;
-export {};
