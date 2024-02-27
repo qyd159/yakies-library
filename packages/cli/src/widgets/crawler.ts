657927c9c432472b736a32ccdf04b0784a6a5f6a
chumahtung <qyd159@163.com>
2024-02-27 09:28:18 +0800

    release: @yakies/cli v0.15.0

--- packages/cli/src/widgets/crawler.ts
+++ packages/cli/src/widgets/crawler.ts
@@ -1,5 +1,9 @@
-const cmd = require('node-cmd-promise');
+import { spawn } from 'child_process';
 
 export default function (args) {
-  cmd(`pnpm exec gpt-crawler`).catch((e) => console.error(e));
+  const crawlerParams = [];
+  crawlerParams.push('-u', args.u || 'https://docs.nvidia.com/metropolis/deepstream/dev-guide/');
+  crawlerParams.push('-m', args.m || 'https://docs.nvidia.com/metropolis/deepstream/dev-guide/');
+  crawlerParams.push('-s', args.s || 'body');
+  spawn(`pnpm`, ['exec', 'gpt-crawler', ...crawlerParams], { stdio: 'inherit' });
 }
