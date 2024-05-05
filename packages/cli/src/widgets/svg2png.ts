eea8fb78e83b16b05572396975303ed9e3fe8c29
chumahtung <qyd159@163.com>
2024-05-05 17:26:49 +0800

    feat: add size option to svg2png widget

--- packages/cli/src/widgets/svg2png.ts
+++ packages/cli/src/widgets/svg2png.ts
@@ -5,12 +5,15 @@ import path from 'path';
 
 export default async function (args) {
   await initialize(readFileSync(path.join(__dirname, '../../node_modules/svg2png-wasm/svg2png_wasm_bg.wasm')));
-
+  let width = 1024,height=1024;
+  if(args.size){
+    [width,height] = args.size.split(',')
+  }
   /** @type {Uint8Array} */
   const png = await svg2png(readFileSync(path.join(process.cwd(), args.f)).toString(), {
     // scale: 2, // optional
-    width: 1024, // optional
-    height: 1024, // optional
+    width, // optional
+    height, // optional
   });
   writeFileSync(args.o || './output.png', png);
 }
