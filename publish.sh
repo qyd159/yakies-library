8147145043ab1f89efdf7cd2910c2adfba7e24ba
qinyadong <qyd159@163.com>
2024-05-16 15:15:23 +0800

    chore: update

--- publish.sh
+++ publish.sh
@@ -12,5 +12,5 @@ fi
 if [ $1 = "all" ]; then
   pnpm publish -r
 else
-  pnpm publish --filter $1 --registry 'https://nexus.yakies.cn/repository/hosted/' --no-git-checks
+  pnpm publish --filter $1 --registry 'https://npm.yakies.cn/repository/hosted/' --no-git-checks
 fi
