0f482e12e62b3fa3b61c6c68b2232d6eb82b5ad4
chumahtung <qyd159@163.com>
2024-05-25 10:43:40 +0800

    chore: remove localStorage dependency of @yakies/http

--- packages/http/src/utils/token.ts
+++ packages/http/src/utils/token.ts
@@ -14,57 +14,49 @@ export interface SearchListRes<T> {
   meta: any;
 }
 
-let ls: any =
-  typeof localStorage === 'undefined'
-    ? (async () => {
-        ls = (await import('localStorage')).default;
-        // 使用导入的模块
-      })()
-    : localStorage;
-
 /**
  * token保存到本地
  * @param token
  */
 export function setTokenToLocal(token: string) {
-  ls.setItem(TOKEN_STORAGE_KEY, token);
+  localStorage.setItem(TOKEN_STORAGE_KEY, token);
 }
 export function setAccessTokenToLocal(token: string) {
-  ls.setItem(TOKEN_ACCESS_STORAGE_KEY, token);
+  localStorage.setItem(TOKEN_ACCESS_STORAGE_KEY, token);
 }
 export function setRefreshTokenToLocal(refreshToken: string) {
-  ls.setItem(TOKEN_REFRESH_STORAGE_KEY, refreshToken);
+  localStorage.setItem(TOKEN_REFRESH_STORAGE_KEY, refreshToken);
 }
 /**
  * 从本地获取token
  */
 export function getTokenFromLocal() {
-  return ls.getItem(TOKEN_STORAGE_KEY);
+  return localStorage.getItem(TOKEN_STORAGE_KEY);
 }
 export function getAccessTokenFromLocal() {
-  return ls.getItem(TOKEN_ACCESS_STORAGE_KEY);
+  return localStorage.getItem(TOKEN_ACCESS_STORAGE_KEY);
 }
 export function getRefreshTokenFromLocal() {
-  return ls.getItem(TOKEN_REFRESH_STORAGE_KEY);
+  return localStorage.getItem(TOKEN_REFRESH_STORAGE_KEY);
 }
 /**
  * 清除本地token
  */
 export function clearToken() {
-  ls.removeItem(TOKEN_STORAGE_KEY);
-  ls.removeItem(USERNAME_STORAGE_KEY);
-  ls.removeItem(TOKEN_REFRESH_STORAGE_KEY);
+  localStorage.removeItem(TOKEN_STORAGE_KEY);
+  localStorage.removeItem(USERNAME_STORAGE_KEY);
+  localStorage.removeItem(TOKEN_REFRESH_STORAGE_KEY);
 }
 
 /**
  * 用户名保存到本地
  */
 export function setUsernameToLocal(username: string) {
-  ls.setItem(USERNAME_STORAGE_KEY, username);
+  localStorage.setItem(USERNAME_STORAGE_KEY, username);
 }
 /**
  * 从本地获取用户名
  */
 export function getUsernameFromLocal() {
-  return ls.getItem(USERNAME_STORAGE_KEY);
+  return localStorage.getItem(USERNAME_STORAGE_KEY);
 }
